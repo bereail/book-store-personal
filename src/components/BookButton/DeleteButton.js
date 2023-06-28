1cd8fb451aa3707dcb03b1fe072f117b23bf24d9
BERE <bereailsolohaga@hotmail.com>
2023-06-28 10:57:24 -0300

    Delete button

--- src/components/BookButton/DeleteButton.js
+++ src/components/BookButton/DeleteButton.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
-const DeleteButton = ({}) => {
+const DeleteButton = ({ bookId }) => {
+  const handleDelete = async () => {
+    // Mostrar una ventana emergente de confirmación antes de eliminar el libro
+    const confirmed = window.confirm("¿Estás seguro de que deseas eliminar este libro?");
+
+    if (confirmed) {
+      try {
+        const bookRef = firebase.firestore().collection("books").doc(bookId);
+
+        // Obtén el título del libro utilizando el ID y verifica si existe
+        const bookDoc = await bookRef.get();
+        if (bookDoc.exists) {
+          await bookRef.delete();
+          alert("Libro eliminado exitosamente.");
+        } else {
+          alert("El libro no existe.");
+        }
+      } catch (error) {
+        alert("Error al eliminar el libro:", error);
+      }
+    }
+  };
+
   return (
-    <Button onClick={onDelete}>
-      Delete
+    <Button variant="danger" onClick={handleDelete}>
+      Eliminar libro
     </Button>
-  )
-} 
+  );
+};
 
 export default DeleteButton;
--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,46 +1,45 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { db } from "../../firebase/config";
 import BookCard from "../Card/Card";
+import { VscBug } from "react-icons/vsc";
+import DeleteButton from "../BookButton/DeleteButton";
 
 const BookList = () => {
-    const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState([]);
 
-    useEffect(() => {
-        //consultar la coleccion"books" en firebase
-        db.collection('books').get()
-        .then((snapshot) => {
-            //Creat un array con los datos de los libros
-            const booksData = snapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data()
-            }));
-            //Actualizar el estado con los componentes del libro
-            setBooks(booksData);
-        })
-        .catch((error) => {
-            console.error("Error al obtener los libros", error);
-        });
-    }, []);
+  useEffect(() => {
+    // Consultar la colección "books" en Firebase
+    db.collection("books")
+      .get()
+      .then((snapshot) => {
+        // Crear un array con los datos de los libros
+        const booksData = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        // Actualizar el estado con los componentes del libro
+        setBooks(booksData);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los libros", error);
+      });
+  }, []);
 
-
-    
-    return (
-      <BookCard>
-        <div>
-          <h2>Lista de libros:</h2>
-          {books.map((book) => (
-            <div key={book.id}>
-              <h3>{book.title}</h3>
-              <p>Autor: {book.author}</p>
-              <p>Páginas: {book.pages}</p>
-              <p>Descripción: {book.description}</p>
-              <img src={book.img} alt="Portada del libro" />
-            </div>
-          ))}
+  return (
+    <div>
+      <h2>Lista de libros:</h2>
+      {books.map((book) => (
+        <div key={book.id}>
+          <h3>{book.title}</h3>
+          <p>Autor: {book.author}</p>
+          <p>Páginas: {book.pages}</p>
+          <p>Descripción: {book.description}</p>
+          <img src={book.img} alt="Portada del libro" />
+          <DeleteButton bookId={book.id} />
         </div>
-        </BookCard>
-      );
-    
-    };
+      ))}
+    </div>
+  );
+};
 
 export default BookList;
