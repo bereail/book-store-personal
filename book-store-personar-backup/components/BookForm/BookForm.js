ccc358abe7451cea7e32c62a075f0da54f9e5a3b
bereail <bereailsolohaga@hotmail.com>
2023-06-30 18:27:57 -0300

    backup-personal

--- book-store-personar-backup/components/BookForm/BookForm.js
+++ book-store-personar-backup/components/BookForm/BookForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./BookItems.css";
+import { Button } from "react-bootstrap";
 import BookCard from "../Card/Card";
 
 
@@ -8,17 +8,21 @@ const BookItem = ({ data }) => {
     return null; // Retorna null o muestra un indicador de carga mientras los datos se están obteniendo
   }
 
-  const { Author, Pages, Title, Img } = data;
+  const { Title, Author, Pages, Description, Img } = data;
 
 
   return (
     <BookCard>
     <div>
+      <h1>{Title}</h1>
       <h1>{Author}</h1>
       <h3>{Pages}</h3>
-      <h1>{Title}</h1>
+      <p>{Description}</p>
       <img className="image" src={Img}  alt="descripcion img"/>
     </div>
+    <Button>
+         Reserved
+        </Button>
     </BookCard>
   );
 };
